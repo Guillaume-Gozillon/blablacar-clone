a2c3be8bee28b541ad8830a33fad771bb993cd99
guillaumegozillon <guillaumegozillon@gmail.com>
2023-07-01 19:08:15 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+import { Typography } from "@mui/material";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Typography>Blablacar</Typography>
+    </>
+  );
+}
+
+export default App;
