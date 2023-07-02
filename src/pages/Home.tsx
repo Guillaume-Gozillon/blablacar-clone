8b58b048adab37d879b90c858befbe63465412db
guillaumegozillon <guillaumegozillon@gmail.com>
2023-07-02 16:49:25 +0200

    Add storybook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,17 @@
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
+// import { Button } from "@mui/material";
+// import { Link } from "react-router-dom";
+import { Button } from "../stories/Button";
 
 const Home = () => {
   return (
     <>
       <h1>Home Page</h1>
-      <Button component={Link} to="/about" variant="contained" color="primary">
-        Go to About
-      </Button>
+      <Button
+        label="Go to About"
+        primary
+        //  component={Link}
+        // to="/"
+      />
     </>
   );
 };
